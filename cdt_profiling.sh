fd480170f257a44467a5c58cbbe2ff74c64efab5
KomeijiForce <lepeng@ucsd.edu>
2026-01-17 23:45:36 +0000

    add auto cdt_profiling

---
+++ cdt_profiling.sh
@@ -0,0 +1,10 @@
+python cdt_profiling.py \
+  --engine "gpt-4.1"\
+  --character_wiki_name 平泽唯 \
+  --lang Chinese \
+  --profiling_character Yui \
+  --main_characters Yui Ritsu Mio Mugi Azusa \
+  --topics identity personality ability relationship \
+  --storyline_path storylines/kon.storyline.part.txt \
+  --output_path profiles/Yui.wikified.profile.txt \
+  --device_id 1
